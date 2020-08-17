a4d2e53b5fb8bf9c031a980ea4851ba3bade230c
TamaraJammoul <tamara.jamoul@gmail.com>
2020-08-18 00:57:33 +0300

    first commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://tamara:includeiostream@cluster0.t87a5.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  JWT_SECRET: "includeiostream",
+  SENDGRID_API: process.env.SENDGRID_API,
+  EMAIL: process.env.EMAIL,
+};
